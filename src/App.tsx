f5c5096ec0a51335683f24b4ffe39201f8d103a8
Loucif <loucif.ft13@gmail.com>
2024-09-15 22:49:43 +0000

    fix : fix none used declared variables

--- src/App.tsx
+++ src/App.tsx
@@ -18,16 +18,18 @@ const axiosInstance = axios.create({
 
 const Profile: React.FC = () => {
   const [data, setData] = useState<Data | null>(null);
-  const [error, setError] = useState<string | null>(null);
+  const [error_, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axiosInstance.get<Data>(import.meta.env.VITE_ENDPOINT);
         setData(JSON.parse(JSON.stringify(response.data))[0]);
-        console.log(JSON.parse(JSON.stringify(response.data))[0]);
       } catch (error) {
         setError("Failed to fetch data");
+      } finally {
+        setLoading(false);
       }
     };
     fetchData();
@@ -35,21 +37,31 @@ const Profile: React.FC = () => {
 
   return (
     <div className="container">
-      <Header name={data?.name} designation={data?.designation} />
-      <div className="row">
-        <div className="col-md-8 content">
-          <Education education={data?.education} />
-          <Experience experiences={data?.experiences} />
-          <Project projects={data?.projects} />
-        </div>
-        <div className="col-md-4 photo">
-          <div className="profil-image"></div>
-          <Contact contact={data?.contact} />
-          <Skill skills={data?.skill} />
-          <Language languages={data?.language} />
-          <Interest interests={data?.interest} />
-        </div>
-      </div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error_ ? (
+        <div>{error_}</div>
+      ) : data ? (
+        <>
+          <Header name={data?.name} designation={data?.designation} />
+          <div className="row">
+            <div className="col-md-8 content">
+              <Education education={data?.education} />
+              <Experience experiences={data?.experiences} />
+              <Project projects={data?.projects} />
+            </div>
+            <div className="col-md-4 photo">
+              <div className="profil-image"></div>
+              <Contact contact={data?.contact} />
+              <Skill skills={data?.skill} />
+              <Language languages={data?.language} />
+              <Interest interests={data?.interest} />
+            </div>
+          </div>
+        </>
+      ) : (
+        <div>No data available</div>
+      )}
     </div>
   );
 };
